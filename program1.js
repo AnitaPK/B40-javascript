7285da3cf19f84a74ad883450b40775d760772eb
AnitaPK <anita.khamitkar@gmail.com>
2024-11-21 12:06:27 +0530

    output and Input in js

---
+++ program1.js
@@ -0,0 +1,9 @@
+console.log("hellow world");
+
+alert("***********hello from external file***********");
+
+nameFromPromt = prompt("Enter your Name")
+
+console.log(nameFromPromt);
+
+document.getElementById('fnameFromPromt').innerHTML = nameFromPromt
